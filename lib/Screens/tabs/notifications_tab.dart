f99ae356affe57ab0545f07c20506780a032e570
MohamedAshraf2 <Hamoudy495@gmail.com>
2021-12-13 04:00:39 +0200

    Failed Test Of FirebaseCloudMessegingNotifications

--- lib/Screens/tabs/notifications_tab.dart
+++ lib/Screens/tabs/notifications_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class NotificationsTab extends StatefulWidget {
   const NotificationsTab({Key key}) : super(key: key);
@@ -8,6 +9,25 @@ class NotificationsTab extends StatefulWidget {
 }
 
 class _NotificationsTabState extends State<NotificationsTab> {
+  Future<void> setupInteractedMessage() async {
+    RemoteMessage initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    print(message.data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupInteractedMessage();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:clinido/navigation/extract_arguments_screen .dart';
 import 'package:clinido/screens/welcome_screen.dart';
 import 'package:clinido/screens/authontication/login_screen.dart';
 import 'package:clinido/screens/authontication/registration_screen.dart';
@@ -8,9 +7,13 @@ import 'package:clinido/screens/home_screen.dart';
 import 'package:clinido/screens/specialities_screen.dart';
 import 'package:clinido/screens/speciality_doctors_screen.dart';
 import 'package:clinido/screens/confirm_reservation_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -20,40 +23,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _initialized = false;
-  bool _error = false;
-
-  void initializeFlutterFire() async {
-    try {
-      // Wait for Firebase to initialize and set `_initialized` state to true
-      await Firebase.initializeApp();
-      setState(() {
-        _initialized = true;
-      });
-    } catch (e) {
-      // Set `_error` state to true if Firebase initialization fails
-      setState(() {
-        _error = true;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    initializeFlutterFire();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.lightBlue),
-      // home: WelcomeScreen(),
-      home: _error
-          ? ErrorWidget()
-          : _initialized
-              ? WelcomeScreen()
-              : Loading(),
+      home: WelcomeScreen(),
       debugShowCheckedModeBanner: false,
       initialRoute: WelcomeScreen.id,
       routes: {
